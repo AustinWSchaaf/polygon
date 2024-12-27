9a517e403b7fa7cfae4e1ae21399034737f0ce7c
Austin Schaaf <austin.schaaf@gmail.com>
2024-12-27 09:54:35 -0600

    Added a few response models

--- polygon.swift
+++ polygon.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 //Main
 
 struct Polygon: Sendable {
@@ -25,13 +26,19 @@ struct Polygon: Sendable {
 struct Stocks {
     private let apiKey: String
     
-    init (apiKey: String) {
+    init(apiKey: String) {
         self.apiKey = apiKey
     }
     
     func aggregates(ticker: String, multiplier: Int, timespan: String, from: String, to: String, adjusted: Bool = true, sort: String = "asc", limit: Int = 5000) async throws -> (Data, URLResponse) {
         let aggs = Aggregates(ticker: ticker, multiplier: multiplier, timespan: timespan, from: from, to: to, adjusted: adjusted, sort: sort, limit: limit)
+        print(aggs.url)
         return try await HTTP.get("\(aggs.url)\(apiKey)")
+    }
+    
+    func groupedBars(date: String, adjusted: Bool = true, includeOTC: Bool = false) async throws -> (Data, URLResponse) {
+        let groupedBars = GroupedBars(date: date, adjusted: adjusted, includeOTC: includeOTC)
+        return try await HTTP.get("\(groupedBars.url)\(apiKey)")
     }
 }
 
@@ -77,14 +84,20 @@ struct Aggregates: Path {
     }
     
     var url: String {
-         "https://api.polygon.io/v2/aggs/ticker/\(ticker)/range/\(multiplier)/day/\(from)/\(to)?adjusted=\(adjusted)&sort=\(sort)&apiKey="
+        "https://api.polygon.io/v2/aggs/ticker/\(ticker)/range/\(multiplier)/\(timespan)/\(from)/\(to)?adjusted=\(adjusted)&sort=\(sort)&apiKey="
     }
 }
 
 struct GroupedBars: Path {
     private let date: String
-    private let adjusted = true
-    private let includeOTC = false
+    private var adjusted = true
+    private var includeOTC = false
+    
+    init(date: String, adjusted: Bool = true, includeOTC: Bool = false) {
+        self.date = date
+        self.adjusted = adjusted
+        self.includeOTC = includeOTC
+    }
     
     var url: String {
         "https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/\(date)?adjusted=\(adjusted)&include_otc=\(false)&apiKey="
@@ -96,12 +109,6 @@ struct DailyOpenClose: Path {
     private let date: String
     private var adjusted = true
     
-    init(ticker: String, date: String, adjusted: Bool = true) {
-        self.ticker = ticker
-        self.date = date
-        self.adjusted = adjusted
-    }
-    
     var url: String {
         "https://api.polygon.io/v2/aggs/daily/\(date)/\(ticker)?adjusted=\(adjusted)&apiKey="
     }
@@ -110,45 +117,38 @@ struct DailyOpenClose: Path {
 struct PreviousClose: Path {
     private let ticker: String
     private var adjusted = true
-    
-    init(ticker: String, adjusted: Bool = true) {
-        self.ticker = ticker
-        self.adjusted = adjusted
-    }
 
     var url: String {
         "https://api.polygon.io/v2/aggs/ticker/\(ticker)/prev?adjusted=\(adjusted)&apiKey="
     }
 }
 
-struct Trades: Path {
+//struct Trades: Path {
+//    private let ticker: String
+//    private var timestamp: PolygonDate?
+//    private var order: String?
+//    private var limit = 1000
+//    private var sort: String?
+//    
+//}
+
+struct LastTrade: Path {
     private let ticker: String
-    private var timestamp: String?
-    private var order: String?
-    private var limit = 1000
-    
     
     var url: String {
-        "https://api.polygon.io/v2/aggs/ticker/\(ticker)/trades?apiKey="
+        "https://api.polygon.io/v2/last/trade/\(ticker)?apiKey="
     }
 }
 
-
-
-//Responses
-
-struct AggregatesResponse: Codable {
-    let resultsCount: Int
-    let results: [Results]
+struct Quotes: Path {
+    private let ticker: String
+    private var timestamp: String?
+    private var order: String?
+    private var limit: Int = 1000
+    private var sort: String?
     
-    struct Results: Codable {
-        let o: Double
-        let h: Double
-        let l: Double
-        let c: Double
-        let n: Int
-        let v: Int
-        let vw: Double
+    var url: String {
+        "https://api.polygon.io/v3/quotes/\(ticker)?order=asc&limit=1000&sort=timestamp&apiKey="
     }
 }
 
@@ -181,19 +181,99 @@ struct HTTP {
     }
 }
 
+enum PolygonTime {
+    case date(String)
+    case timestamp(Int)
+    
+    func rawValue() -> String {
+        switch self {
+        case .date(let date):
+            return date
+        case .timestamp(let timestamp):
+            return String(timestamp)
+        }
+    }
+}
+
+class PolygonDecoder: JSONDecoder {
+    override init() {
+        super.init()
+        keyDecodingStrategy = .convertFromSnakeCase
+    }
+}
+
+extension PolygonDecoder: @unchecked Sendable {
+    
+}
+
+//Responses
+
+struct BarData: Codable {
+    let T: String?
+    let o: Double
+    let h: Double
+    let l: Double
+    let c: Double
+    let t: Int
+    let n: Int
+    let v: Int
+    let vw: Double
+}
+
+struct AggregatesResponse: Codable {
+    let adjusted: Bool
+    let nextUrl: String?
+    let queryCount: Int
+    let requestId: String
+    let results: [BarData]
+    let resultsCount: Int
+    let status: String
+    let ticker: String
+}
+
+struct GroupedBarsResponse: Codable {
+    let adjusted: Bool
+    let queryCount: Int
+    let results: [BarData]
+    let resultsCount: Int
+    let status: String
+}
+
+struct DailyOpenCloseResponse: Codable {
+    let afterHours: Double
+    let close: Double
+    let from: String
+    let high: Double
+    let low: Double
+    let open: Double
+    let premarket: Double
+    let status: String
+    let symbol: String
+    let volume: Int
+}
+
+struct PreviousCloseResponse: Codable {
+    let adjusted: Bool
+    let queryCount: Int
+    let requestId: String
+    let results: [BarData]
+    let resultsCount: Int
+    let status: String
+    let ticker: String
+}
+
+let apiKey = ""
 let polygon = Polygon(apiKey: apiKey)
 do {
-    let (data, response) = try await polygon.stocks.aggregates(ticker: "AAPL",
+    let (data, _) = try await polygon.stocks.aggregates(ticker: "AAPL",
                                                                multiplier: 1,
-                                                               timespan: PolygonTimespan.day,
+                                                               timespan: PolygonTimespan.minute,
                                                                from: "2023-02-09",
-                                                               to: "2023-02-10")
+                                                               to: "2023-02-13")
 
 
-    let aggs = try JSONDecoder().decode(AggregatesResponse.self, from: data)
-    for result in aggs.results {
-        print(result)
-    }
+    let aggs = try PolygonDecoder().decode(AggregatesResponse.self, from: data)
+    print(aggs.resultsCount)
 } catch let error {
     print(error)
 }
